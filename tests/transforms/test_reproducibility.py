6c3fe41f2d27452415a168f3dcf395383c26e1ec
Fernando <fepegar@gmail.com>
2020-07-12 15:25:28 +0100

    Add reproducibility tests

--- tests/transforms/test_reproducibility.py
+++ tests/transforms/test_reproducibility.py
@@ -1,5 +1,7 @@
+import warnings
 import torch
-from torchio import Subject, Image
+import torchio
+from torchio import Subject, Image, INTENSITY
 from torchio.transforms import RandomNoise
 from ..utils import TorchioTestCase
 
@@ -47,3 +49,41 @@ class TestReproducibility(TorchioTestCase):
         self.assertEqual(seed_c2, seed_a2)
         self.assertEqual(c2, c)
         self.assertEqual(seed_c2, seed_c)
+
+    def test_all_random_transforms(self):
+        sample = Subject(
+            t1=Image(tensor=torch.rand(20, 20, 20)),
+            seg=Image(tensor=torch.rand(20, 20, 20) > 1, type=INTENSITY)
+        )
+
+        transforms_names = [
+            name
+            for name in dir(torchio)
+            if name.startswith('Random')
+        ]
+
+        # Downsample at the end so that the image shape is not modified
+        transforms_names.remove('RandomDownsample')
+        transforms_names.append('RandomDownsample')
+
+        transforms = []
+        for transform_name in transforms_names:
+            transform = getattr(torchio, transform_name)()
+            transforms.append(transform)
+        composed_transform = torchio.Compose(transforms)
+        with warnings.catch_warnings():  # ignore elastic deformation warning
+            warnings.simplefilter('ignore', UserWarning)
+            transformed = composed_transform(sample)
+
+        new_transforms = []
+        for transform_name, params_dict in transformed.history:
+            transform_class = getattr(torchio, transform_name)
+            transform = transform_class(seed=params_dict['seed'])
+            new_transforms.append(transform)
+        composed_transform = torchio.Compose(transforms)
+        with warnings.catch_warnings():  # ignore elastic deformation warning
+            warnings.simplefilter('ignore', UserWarning)
+            new_transformed = composed_transform(sample)
+
+        self.assertTensorEqual(transformed.t1.data, new_transformed.t1.data)
+        self.assertTensorEqual(transformed.seg.data, new_transformed.seg.data)
